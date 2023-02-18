b78ac33fa31e50d3c3fad43a0feae0051b7e1dd2
20695A0518 <kundharapuvasudeva@gmail.com>
2023-02-18 12:24:24 +0530

    message

---
+++ src/main/java/com/example/TeamIND/TeamIND/TeamIndApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeamIND.TeamIND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamIndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamIndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TeamIND/TeamIND/TeamIndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeamIND.TeamIND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamIndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
